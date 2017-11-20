cdf4af85ce4b19b7a35fc3a866d23be32c907d44
Remp <KirilRempov@gmail.com>
2017-11-20 22:35:47 +0200

    finish with server

--- src/server.js
+++ src/server.js
@@ -1,4 +1,16 @@
 import Server from "socket.io";
-export function startServer(){
+
+export function startServer(store){
     const io = new Server().attach("8090");
+
+    //при каждом изменении state всем клиентам шлем новый state
+    store.subscribe(() => {
+        io.emit("state", store.getState().toJS());
+    })
+
+    //при подключении отослать текущий state
+    io.on('conneсtion', socket => {
+        socket.emit("state", store.getState().toJS());
+        socket.on("action", store.dispatch.bind(store));
+    })
 }
